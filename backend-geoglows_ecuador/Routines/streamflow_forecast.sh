69a23bf1a01418a0e6e047276b2550e67daa860b
Juseth Chancay <juseth.chancay@gmail.com>
2023-04-28 22:19:25 -0500

    Fixed the conda environment activation command

--- backend-geoglows_ecuador/Routines/streamflow_forecast.sh
+++ backend-geoglows_ecuador/Routines/streamflow_forecast.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
-# Enviromental variables
-ROOT_PATH=~/tethys_apps_ecuador/backend-geoglows_ecuador/Streamflow
+# Environmental variables
+DIR_PATH=~/tethys_apps_ecuador/backend-geoglows_ecuador/Streamflow
 CONDA_ENV=geoglows_ecuador
 
-# Change to work directory
-cd $ROOT_PATH
+# Change work directory
+cd $DIR_PATH
 
-# Activate conda enviroment
-conda activate $CONDA_ENV
+# Activate the conda enviroment
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate geoglows_ecuador
 
-# Print initial timestamp
-date +"%Y-%m-%d %H:%M:%S" >> ${ROOT_PATH}/forecast_db.log
-
-# Download forecast data
-python r_forecast_db.py >> ${ROOT_PATH}/forecast_db.log
-
-# Analize the forecast
-python r_analysis_forecast.py >> ${ROOT_PATH}/forecast_db.log
-
-# Finish the routine
-echo -e "\n" >> ${ROOT_PATH}/forecast_db.log
+# Download and analyze geoglows forecasts
+date +"%Y-%m-%d %H:%M:%S" >> ${DIR_PATH}/forecast_db.log
+python r_forecast_db.py >> ${DIR_PATH}/forecast_db.log
+python r_analysis_forecast.py >> ${DIR_PATH}/forecast_db.log
--- backend-geoglows_ecuador/Routines/streamflow_forecast_record.sh
+++ backend-geoglows_ecuador/Routines/streamflow_forecast_record.sh
@@ -4,17 +4,12 @@
 ROOT_PATH=~/tethys_apps_ecuador/backend-geoglows_ecuador/Streamflow
 CONDA_ENV=geoglows_ecuador
 
-# Change to work directory
+# Change work directory
 cd $ROOT_PATH
 
-# Activate conda enviroment
-conda activate $CONDA_ENV
+# Activate the conda enviroment
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate geoglows_ecuador
 
-# Print initial timestamp
 date +"%Y-%m-%d %H:%M:%S" >> ${ROOT_PATH}/forecast_records.log
-
-# Download forecast records
 python r_forecast_record_db.py >> ${ROOT_PATH}/forecast_records.log
-
-# Finish the routine
-echo -e "\n" >> ${ROOT_PATH}/forecast_records.log
