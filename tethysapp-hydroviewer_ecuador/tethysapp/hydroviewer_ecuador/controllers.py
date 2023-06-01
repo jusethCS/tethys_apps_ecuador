f63eebda3f6bbb7c268e4fbbd6e0405aee54258f
Juseth Chancay <juseth.chancay@gmail.com>
2023-06-01 17:16:55 -0500

    Fixed the env file

--- tethysapp-hydroviewer_ecuador/tethysapp/hydroviewer_ecuador/controllers.py
+++ tethysapp-hydroviewer_ecuador/tethysapp/hydroviewer_ecuador/controllers.py
@@ -42,10 +42,6 @@ DB_USER = os.getenv('DB_USER')
 DB_PASS = os.getenv('DB_PASS')
 DB_NAME = os.getenv('DB_NAME')
 
-DB_USER="geoglows_ecuador"
-DB_PASS="geoglowsRDS2789"
-DB_NAME="geoglows_ecuador"
-
 # Generate the conection token
 global tokencon
 tokencon = "postgresql+psycopg2://{0}:{1}@localhost:5432/{2}".format(DB_USER, DB_PASS, DB_NAME)
